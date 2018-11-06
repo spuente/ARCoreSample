f4274df9b93f6aeb33a06ccb1532f195a76051b8
Sebastian Puente <sebas7pr@gmail.com>
2018-11-06 16:28:34 -0500

    Adds ArFragment to layout and configures required permissions and metadata

--- app/src/main/java/com/example/spuente/arcoresample/MainActivity.kt
+++ app/src/main/java/com/example/spuente/arcoresample/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.spuente.arcoresample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.ar.sceneform.ux.ArFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var arFragment: ArFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        arFragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
     }
 }
