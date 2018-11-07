57b2dd9b46137182811b5224132c9096bf7fff56
Sebastian Puente <sebas7pr@gmail.com>
2018-11-07 12:20:19 -0500

    Adds letter C and displays it next to B when user taps on screen

--- app/src/main/java/com/example/spuente/arcoresample/MainActivity.kt
+++ app/src/main/java/com/example/spuente/arcoresample/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var arFragment: ArFragment
     private lateinit var letterRenderable1: ModelRenderable
     private lateinit var letterRenderable2: ModelRenderable
+    private lateinit var letterRenderable3: ModelRenderable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +33,12 @@ class MainActivity : AppCompatActivity() {
             .thenAccept { this.letterRenderable2 = it }
             .exceptionally { null }
 
+        ModelRenderable.builder()
+            .setSource(this, R.raw.letters_3)
+            .build()
+            .thenAccept { this.letterRenderable3 = it }
+            .exceptionally { null }
+
         arFragment.arSceneView.planeRenderer.isEnabled = true
 
         arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
@@ -46,14 +53,21 @@ class MainActivity : AppCompatActivity() {
             node1.setParent(parentNode)
             node1.renderable = letterRenderable1
 
-            val x1 = parentNode.children[0].localPosition.x + 0.1f
-            val y1 = parentNode.children[0].localPosition.y + 0.0f
-            val z1 = parentNode.children[0].localPosition.z + 0.0f
+            val x1 = parentNode.children[1].localPosition.x + 0.1f
+            val y1 = parentNode.children[1].localPosition.y + 0.0f
+            val z1 = parentNode.children[1].localPosition.z + 0.0f
             val node2 = Node()
             node2.setParent(parentNode)
             node2.localPosition = Vector3(x1, y1, z1)
             node2.renderable = letterRenderable2
 
+            val x2 = parentNode.children[2].localPosition.x + 0.1f
+            val y2 = parentNode.children[2].localPosition.y + 0.0f
+            val z2 = parentNode.children[2].localPosition.z + 0.0f
+            val node3 = Node()
+            node3.setParent(parentNode)
+            node3.localPosition = Vector3(x2, y2, z2)
+            node3.renderable = letterRenderable3
         }
     }
 }
